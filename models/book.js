38c92430bb07ebdf5917187ce675522128d66afe
AJ TECH <anajadaniel50@gmail.com>
2026-04-09 16:33:09 +0100

    third commit

--- models/book.js
+++ models/book.js
@@ -1,22 +1,48 @@
 const mongoose = require("mongoose");
 
 const bookSchema = new mongoose.Schema({
-    title: {type: String, required: true},
-
-    isbn: {type: String, unique: true},
-
-    author: [{type: mongoose.Schema.Types.ObjectId, ref:"author"}],
-
-    status: {type: String,
-        enum:["IN","OUT"],
-        default: "IN",
+    title: {
+        type: String,
+        required: [true, "Title is required"],
+        trim: true,
+        maxlength: [200, 'Title cannot exceed 200 characters']
     },
 
-    borrowedBy: {type: mongoose.Schema.Types.ObjectId, ref:"student"},
+    authors: [{type: mongoose.Schema.Types.ObjectId, ref: "authors", required: true}],
 
-    issuedBy: {type: mongoose.Schema.Types.ObjectId, ref:"attendant"},
+    isbn: {
+        type: String,
+        unique: true,
+        required: [true, 'ISBN is required']
+    },
 
-    returnDate: {type: Date, default: null},
-}, {timestamps: true})
+    status: {
+        type: String,
+        enum: ["IN", "OUT"],
+        default: "IN"
+    },
 
-module.exports = mongoose.model("book", bookSchema);
+    borrowedBy: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "student",
+        default: null
+    },
+
+    issuedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "librarian",
+        default: null
+    },
+
+    returnDate: {
+        type: Date,
+        default: null
+    },
+}, 
+
+{ //schema options
+    timestamps: true
+
+})
+
+module.exports = mongoose.model("Book", bookSchema);
