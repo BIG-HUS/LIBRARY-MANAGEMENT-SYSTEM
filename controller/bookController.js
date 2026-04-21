fe99103f5becb51f12bbd71854266a1b898e819c
AJ TECH <anajadaniel50@gmail.com>
2026-04-21 01:21:00 +0100

    Added createBook, getAllBooks, getBookById

--- controller/bookController.js
+++ controller/bookController.js
@@ -2,29 +2,94 @@ const Book = require("../models/book");
 const Author = require("../models/author");
 
 const createBook = async (req,res) => {
-    const{title,isbn,author} = req.body
 
-    if(!title || !isbn) {
-        return res.status(400).json({error: "Title and ISBN must be filled"}) 
+    try{
+        const{title,isbn,author} = req.body
+
+        if(!title || !isbn) {
+            return res.status(400).json({error: "Title and ISBN must be filled"}) 
+        }
+
+        const checkTitle = await Book.findOne({title: title});
+        const checkIsbn = await Book.findOne({ISBN: isbn});
+        //const checkAuthor = await Book.findOne({Author: author})
+
+        if(checkTitle && checkIsbn) {
+            return res.status(400).json({error: "Book already exists"});
+        }
+
+        const newBook = await Book.create({...req.body, AuthorId: author});
+
+        res.status(200).json({
+            message: "Book created successfully",
+            data: newBook
+        });
     }
 
-    const checkTitle = await Book.findOne({title: title});
-    const checkIsbn = await Book.findOne({ISBN: isbn});
-    //const checkAuthor = await Book.findOne({Author: author})
+    catch(err){
+        res.status(500).json({error: err.message})
+    }
+    
+}
 
-    if(checkTitle && checkIsbn) {
-        return res.status(400).json({error: "Book already exists"});
+
+const getAllBooks = async (req,res) => {
+
+    try{
+        const {page=1,limit=10} = req.body;
+
+        const skip = (page-1) * 10;
+
+        const getBooks = await Book.find()
+        .limit(parseInt(limit))
+        .skip(parseInt(skip))
+        .select('-__v')
+        .exec()
+
+        if(!getBooks){
+            return res.status(400).json({error: "Books not available"})
+        };
+
+        res.status(200).json({
+            message: "Books retrieved successfully",
+            data: getBooks,
+            pagination: {
+                page: parseInt(page),
+                limit: parseInt(limit)
+            }
+        });
     }
 
-    const newBook = await Book.create({...req.body, AuthorId: author});
+    catch(err){
+        res.status(500).json({error: err.message})
+    }
+}
 
-    res.status(200).json({
-        message: "Book created successfully",
-        data: newBook
-    });
+
+const getBookById = async (req,res) => {
+    try{
+        const bookId = req.params.id;
+
+        const getBook = await Book.findById(bookId);
+
+        if(!getBook){
+            return res.status(400).json({error: "Book does not exist"});
+        }
+
+        res.status(200).json({
+            message: "Book retrieved successfully",
+            data: getBook
+        })
+    }
+
+    catch(err){
+        res.status(500).json({error: err.message})
+    }
 }
 
 
 module.exports = {
     createBook,
+    getAllBooks,
+    getBookById
 }
--- routes/book.route.js
+++ routes/book.route.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router()
 
-const{createBook} = require("../controller/bookController");
+const{createBook,getAllBooks,getBookById} = require("../controller/bookController");
 
 router.post('/', createBook);
+router.get('/', getAllBooks);
+router.get('/:id', getBookById)
 
 module.exports = router
